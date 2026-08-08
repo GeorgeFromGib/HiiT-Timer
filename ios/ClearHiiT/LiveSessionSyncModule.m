c1003f7054e489e50139876e127e541635544faf
george <george_gaskin@msn.com>
2026-08-08 20:24:20 +0200

    feat: receive watch-originated live session updates on the phone

--- ios/ClearHiiT/LiveSessionSyncModule.m
+++ ios/ClearHiiT/LiveSessionSyncModule.m
@@ -1,6 +1,7 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(LiveSessionSync, NSObject)
+@interface RCT_EXTERN_MODULE(LiveSessionSync, RCTEventEmitter)
 
 RCT_EXTERN_METHOD(updateLiveSession:(NSString *)sessionId name:(NSString *)name elapsed:(double)elapsed status:(NSString *)status)
 
--- ios/ClearHiiT/LiveSessionSyncModule.swift
+++ ios/ClearHiiT/LiveSessionSyncModule.swift
@@ -1,10 +1,24 @@
 // ios/ClearHiiT/LiveSessionSyncModule.swift
 import Foundation
+import React
 import WatchConnectivity
 
 @objc(LiveSessionSync)
-class LiveSessionSync: NSObject, WCSessionDelegate {
+class LiveSessionSync: RCTEventEmitter, WCSessionDelegate {
   private static var didActivate = false
+  private var hasListeners = false
+
+  override func supportedEvents() -> [String]! {
+    ["LiveSessionUpdate"]
+  }
+
+  override func startObserving() {
+    hasListeners = true
+  }
+
+  override func stopObserving() {
+    hasListeners = false
+  }
 
   private func ensureActivated() {
     guard WCSession.isSupported(), !LiveSessionSync.didActivate else { return }
@@ -25,6 +39,9 @@ class LiveSessionSync: NSObject, WCSessionDelegate {
       "updatedAt": Date().timeIntervalSince1970,
     ]
     try? WCSession.default.updateApplicationContext(context)
+    if WCSession.default.isReachable {
+      WCSession.default.sendMessage(context, replyHandler: nil, errorHandler: nil)
+    }
   }
 
   @objc
@@ -34,14 +51,29 @@ class LiveSessionSync: NSObject, WCSessionDelegate {
     try? WCSession.default.updateApplicationContext([:])
   }
 
+  private func forwardToJS(_ payload: [String: Any]) {
+    guard hasListeners else { return }
+    DispatchQueue.main.async { [weak self] in
+      self?.sendEvent(withName: "LiveSessionUpdate", body: payload)
+    }
+  }
+
   func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: (any Error)?) {}
   func sessionDidBecomeInactive(_ session: WCSession) {}
   func sessionDidDeactivate(_ session: WCSession) {
     WCSession.default.activate()
   }
 
+  func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String: Any]) {
+    forwardToJS(applicationContext)
+  }
+
+  func session(_ session: WCSession, didReceiveMessage message: [String: Any]) {
+    forwardToJS(message)
+  }
+
   @objc
-  static func requiresMainQueueSetup() -> Bool {
+  override static func requiresMainQueueSetup() -> Bool {
     return false
   }
 }
